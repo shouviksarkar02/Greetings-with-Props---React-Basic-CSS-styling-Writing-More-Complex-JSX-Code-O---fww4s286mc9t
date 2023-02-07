81f67e898ac9a718912a7f4c006942d7b99b51d9
shouvik sarkar <shouviksarkar2001@gmail.com>
2023-02-08 00:14:12 +0530

    hiee

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,11 +1,11 @@
 // write code for Welcome component here
 import React from "react";
 const Welcome =(props) => {
-    return{
+    return(
         <div>
         <h1>"Hey {props.name}!"</h1>
         <h2>"Welcome to Newton School."</h2>
         </div>
-    }
+    )
 }
 export default Welcome;
